2ceaab7995513c898fa07205cccb61e29ed7ccd9
halik <halik@Atanda>
2022-11-10 07:37:26 +0100

    Creation de command et la classe qui l'implémente

--- src/fr/istic/aco/editor/SelectionImpl.java
+++ src/fr/istic/aco/editor/SelectionImpl.java
@@ -37,19 +37,15 @@ public class SelectionImpl implements Selection {
 
 	@Override
 	public void setBeginIndex(int beginIndex) {
-		if (beginIndex > this.getBufferEndIndex())
-			throw new IllegalArgumentException("L'index de debut doit etre inferieur à l'index de fin");
-		if (beginIndex < this.getBufferBeginIndex())
-			throw new IllegalArgumentException("L'index de debut doit etre superieur à 0");
+		if (beginIndex > this.endIndex || beginIndex < BUFFER_BEGIN_INDEX || beginIndex > getBufferEndIndex())
+			throw new IndexOutOfBoundsException();
 		this.beginIndex = beginIndex;
 	}
 
 	@Override
 	public void setEndIndex(int endIndex) {
-		if (this.getBeginIndex() > endIndex)
-			throw new IllegalArgumentException("L'index de fin doit etre superieur à l'index de debut");
-		if (this.getBufferEndIndex() < endIndex)
-			throw new IllegalArgumentException("L'index de fin doit etre inferieur à la taille du buffer");
+		if (endIndex < this.beginIndex || endIndex > getBufferEndIndex() || endIndex < BUFFER_BEGIN_INDEX)
+			throw new IndexOutOfBoundsException();
 		this.endIndex = endIndex;
 	}
 
