65de9008d04890e88fc513369ef6f8cf626bb52c
Halik <abdoul-halik-atanda.kitoyi@etudiant.univ-rennes1.fr>
2022-12-20 12:31:39 +0100

    adding commands tests and minor changes

---
+++ src/fr/istic/aco/editor/test/CommandTest.java
@@ -0,0 +1,182 @@
+package fr.istic.aco.editor.test;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import fr.istic.aco.editor.*;
+import fr.istic.aco.editor.command.*;
+import fr.istic.aco.editor.recorder.Recorder;
+import fr.istic.aco.editor.recorder.RecorderImpl;
+import fr.istic.aco.editor.userinterface.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.ByteArrayInputStream;
+
+import fr.istic.aco.editor.util.Command;
+
+public class CommandTest {
+
+	private UserInterface editor;
+
+	private Engine engine;
+
+	private Recorder recorder;
+
+	private String contentExample;
+
+	private void setReadStream(String s) {
+		editor.setReadStream(new ByteArrayInputStream(s.getBytes()));
+	}
+
+	@org.junit.jupiter.api.BeforeEach
+	void setUp() {
+
+		engine = new EngineImpl();
+		editor = new UserInterfaceImpl();
+		recorder = new RecorderImpl();
+
+		editor.addCommand(Command.INSERT, new Insert(engine, editor, recorder));
+		editor.addCommand(Command.SELECT, new SelectionChange(engine, editor, recorder));
+		editor.addCommand(Command.CUT, new CutSelectedText(engine, recorder));
+		editor.addCommand(Command.COPY, new CopySelectedText(engine, recorder));
+		editor.addCommand(Command.PASTE, new PasteClipboard(engine, recorder));
+		editor.addCommand(Command.DELETE, new Delete(engine, recorder));
+
+		contentExample = "Toto likes football";
+	}
+
+	@Test
+	@DisplayName("Begin index and end index must be just after the inserted text")
+	void insert() {
+
+		setReadStream(contentExample);
+
+		editor.executeCommand(Command.INSERT);
+
+		assertEquals(19, engine.getSelection().getBeginIndex());
+		assertEquals(19, engine.getSelection().getEndIndex());
+	}
+
+	@Test
+	@DisplayName("The buffer contains the inserted text")
+	void getBufferContents() {
+
+		setReadStream(contentExample);
+
+		editor.executeCommand(Command.INSERT);
+
+		assertEquals(contentExample, engine.getBufferContents());
+	}
+
+	@Test
+	@DisplayName("Buffer & clipboard must be empty after initialisation")
+	void getSelection() {
+
+		setReadStream(contentExample);
+
+		editor.executeCommand(Command.INSERT);
+
+		setReadStream("11\n15");
+
+		editor.executeCommand(Command.SELECT);
+
+		assertEquals(11, engine.getSelection().getBeginIndex());
+		assertEquals(15, engine.getSelection().getEndIndex());
+	}
+
+	@Test
+	@DisplayName("The cut text is erased from the buffer")
+	void SelectedText() {
+		getSelection();
+
+		// cut it
+		editor.executeCommand(Command.CUT);
+
+		// "foot" must be erased from the buffer
+		assertEquals("Toto likes ball", engine.getBufferContents());
+		assertEquals("foot", engine.getClipboardContents());
+	}
+
+	@Test
+	@DisplayName("The buffer remains unchanged after a copy")
+	void copySelectedText() {
+
+		getSelection();
+
+		// copy it
+		editor.executeCommand(Command.COPY);
+
+		// the buffer remains unchanged
+		assertEquals(contentExample, engine.getBufferContents());
+		assertEquals("foot", engine.getClipboardContents());
+	}
+
+	@Test
+	@DisplayName("The text in the clipboard replaces the current selected text")
+	void pasteClipboard() {
+
+		setReadStream(contentExample + "basket");
+		editor.executeCommand(Command.INSERT);
+
+		assertEquals("Toto likes footballbasket", engine.getBufferContents());
+
+		// select "basket"
+		setReadStream("19\n25");
+		editor.executeCommand(Command.SELECT);
+
+		// cut it
+		editor.executeCommand(Command.CUT);
+
+		// select "basket"
+		setReadStream("11\n15");
+		editor.executeCommand(Command.SELECT);
+
+		// paste it
+		editor.executeCommand(Command.PASTE);
+
+		// "basket" must replace "foot"
+		assertEquals("Toto likes basketball", engine.getBufferContents());
+		assertEquals("basket", engine.getClipboardContents());
+	}
+
+	@Test
+	@DisplayName("The current selected text appear in the clipboard after a copy")
+	void getClipboardContents() {
+
+		setReadStream(contentExample);
+		editor.executeCommand(Command.INSERT); // "Toto likes football"
+
+		// select the word "likes"
+		setReadStream("5\n10");
+		editor.executeCommand(Command.SELECT);
+
+		// copy (or cut) it
+		editor.executeCommand(Command.COPY);
+
+		// verify if the copied word appear in the clipboard
+		assertEquals("likes", engine.getClipboardContents());
+	}
+
+	@Test
+	@DisplayName("Delete has no effect when beginIndex equals to endIndex")
+	void delete() {
+
+		// delete has no effect on empty buffer (after initialisation)
+		editor.executeCommand(Command.DELETE);
+
+		assertEquals("", engine.getBufferContents());
+
+		// delete has no effect when beginIndex == endIndex
+
+		setReadStream(contentExample); // "Toto likes football"
+		editor.executeCommand(Command.INSERT);
+		setReadStream("5\n5");
+		editor.executeCommand(Command.SELECT);
+		engine.delete();
+
+		assertEquals(contentExample, engine.getBufferContents());
+	}
+
+	// TODO Exceptions thrown tests
+}
--- src/fr/istic/aco/editor/test/EngineTest.java
+++ src/fr/istic/aco/editor/test/EngineTest.java
@@ -13,30 +13,34 @@ class EngineTest {
 
 	private Engine engine;
 
-	private static final String BUFFER_CONTENT = "Toto likes football";
+	private String contentExample;
 
 	@org.junit.jupiter.api.BeforeEach
 	void setUp() {
+		
 		engine = new EngineImpl();
+		
+		contentExample = "Toto likes football";
 	}
 
 	@Test
 	@DisplayName("Buffer & clipboard must be empty after initialisation")
 	void getSelection() {
+		
 		Selection selection = engine.getSelection();
 
 		assertEquals(selection.getBufferBeginIndex(), selection.getBeginIndex());
 		assertEquals("", engine.getBufferContents());
 		assertEquals("", engine.getClipboardContents());
-
 	}
 
 	@Test
 	@DisplayName("Begin index and end index must be just after the inserted text")
 	void insert() {
+		
 		Selection selection = engine.getSelection();
 
-		engine.insert(BUFFER_CONTENT);
+		engine.insert(contentExample);
 
 		assertEquals(19, selection.getBeginIndex());
 		assertEquals(19, selection.getEndIndex());
@@ -45,15 +49,18 @@ class EngineTest {
 	@Test
 	@DisplayName("The buffer contains the inserted text")
 	void getBufferContents() {
-		engine.insert(BUFFER_CONTENT);
-		assertEquals(BUFFER_CONTENT, engine.getBufferContents());
+		
+		engine.insert(contentExample);
+		
+		assertEquals(contentExample, engine.getBufferContents());
 	}
 
 	@Test
 	@DisplayName("The cut text is erased from the buffer")
 	void cutSelectedText() {
+		
 		Selection selection = engine.getSelection();
-		engine.insert(BUFFER_CONTENT);
+		engine.insert(contentExample);
 
 		// select "foot" in "football"
 		selection.setBeginIndex(11);
@@ -70,26 +77,29 @@ class EngineTest {
 	@Test
 	@DisplayName("The buffer remains unchanged after a copy")
 	void copySelectedText() {
+		
 		Selection selection = engine.getSelection();
-		engine.insert(BUFFER_CONTENT);
-
+		engine.insert(contentExample)
+		;
 		// select "foot" in "football"
 		selection.setBeginIndex(11);
 		selection.setEndIndex(15);
-
+		
 		// copy it
 		engine.copySelectedText();
-
+		
 		// the buffer remains unchanged
-		assertEquals(BUFFER_CONTENT, engine.getBufferContents());
+		assertEquals(contentExample, engine.getBufferContents());
 		assertEquals("foot", engine.getClipboardContents());
 	}
 
 	@Test
 	@DisplayName("The text in the clipboard replaces the current selected text")
 	void pasteClipboard() {
+		
 		Selection selection = engine.getSelection();
-		engine.insert(BUFFER_CONTENT + "basket");
+		engine.insert(contentExample + "basket");
+		
 		assertEquals("Toto likes footballbasket", engine.getBufferContents());
 
 		// select "basket"
@@ -114,8 +124,9 @@ class EngineTest {
 	@Test
 	@DisplayName("The current selected text appear in the clipboard after a copy")
 	void getClipboardContents() {
+		
 		Selection selection = engine.getSelection();
-		engine.insert(BUFFER_CONTENT); // "Toto likes football"
+		engine.insert(contentExample); // "Toto likes football"
 
 		// select the word "likes"
 		selection.setBeginIndex(5);
@@ -127,53 +138,37 @@ class EngineTest {
 		// verify if the copied word appear in the clipboard
 		assertEquals("likes", engine.getClipboardContents());
 	}
-	
+
 	@Test
 	@DisplayName("Delete has no effect when beginIndex equals to endIndex")
 	void delete() {
+		
 		// delete has no effect on empty buffer (after initialisation)
 		engine.delete();
-		assertEquals("", engine.getBufferContents());
 		
+		assertEquals("", engine.getBufferContents());
+
 		// delete has no effect when beginIndex == endIndex
 		Selection selection = engine.getSelection();
-		engine.insert(BUFFER_CONTENT); // "Toto likes football"
+		engine.insert(contentExample); // "Toto likes football"
 		selection.setBeginIndex(5);
 		selection.setEndIndex(5);
 		engine.delete();
-		assertEquals(BUFFER_CONTENT, engine.getBufferContents());	
+		
+		assertEquals(contentExample, engine.getBufferContents());
 	}
 
 	@Test
 	@DisplayName("Selection out of the buffer bounds")
 	void indexOutOfBounds() throws IndexOutOfBoundsException {
+		
 		Selection selection = engine.getSelection();
 
-		engine.insert(BUFFER_CONTENT);
+		engine.insert(contentExample);
 		assertThrows(IndexOutOfBoundsException.class, () -> selection.setEndIndex(100));
 		assertThrows(IndexOutOfBoundsException.class, () -> selection.setEndIndex(-5));
 
 		assertThrows(IndexOutOfBoundsException.class, () -> selection.setBeginIndex(100));
 		assertThrows(IndexOutOfBoundsException.class, () -> selection.setBeginIndex(-17));
-	}
-
-	@Test
-	@DisplayName("Selection with reversed indexes")
-	void reversedIndexes() {
-		Selection selection = engine.getSelection();
-		engine.insert(BUFFER_CONTENT);
-
-		// Both indexes are in the end of buffers
-		// Move beginIndex before endIndex
-		selection.setBeginIndex(5);
-		selection.setEndIndex(5);
-
-		// EndIndex < current beginIndex = 5
-		selection.setEndIndex(3);
-		assertEquals(3, selection.getBeginIndex());
-
-		// BeginIndex > current endIndex = 5
-		selection.setBeginIndex(8);
-		assertEquals(8, selection.getEndIndex());
 	}
 }
